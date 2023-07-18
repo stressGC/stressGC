0a763388cc1d07defb937d7b52bb0004ca84fb3d
stressgc <17021662+stressGC@users.noreply.github.com>
2023-07-18 09:56:12 +0200

    feat: simple portoflio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ["./src/index.html"],
+	theme: {
+		colors: {
+			offwhite: "#dcdcdc",
+			lightgrey: "#a9a9a9",
+			cyan: "#37a0f0",
+			brightblue: "#0066ff",
+			navy: "#020c1b",
+		},
+	},
+}
